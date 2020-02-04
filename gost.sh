26ab0f2301507a0cf22defc6c689beed98c792db
fly97 <wf.09@qq.com>
2020-02-04 20:24:38 +0800

    update gost.sh

--- gost.sh
+++ gost.sh
@@ -214,9 +214,6 @@ Set_gost_Protocol(){
 	fi
 	echo && echo ${Separator_1} && echo -e " 协议类型 : ${Green_font_prefix}${ssr_method}${Font_color_suffix}" && echo ${Separator_1} && echo
 } 
- 
-}
-
 Set_conf(){
 	Set_ip
 	Set_port
@@ -372,7 +369,6 @@ View_gost(){
 		# echo -e '#!/bin/bash\n/sbin/iptables-restore < /etc/iptables.up.rules' > /etc/network/if-pre-up.d/iptables
 		# chmod +x /etc/network/if-pre-up.d/iptables
 	# fi
-}
 Update_Shell(){
 	sh_new_ver=$(wget --no-check-certificate -qO- -t1 -T3 "${shell_url}" |grep 'sh_ver="'|awk -F "=" '{print $NF}'|sed 's/\"//g'|head -1) && sh_new_type="github"
 	[[ -z ${sh_new_ver} ]] && echo -e "${Error} 无法链接到 Github !" && exit 0
@@ -384,22 +380,21 @@ Update_Shell(){
 	echo -e "脚本已更新为最新版本[ ${sh_new_ver} ] !(注意：因为更新方式为直接覆盖当前运行的脚本，所以可能下面会提示一些报错，无视即可)" && exit 0
 }
 main(){
-	echo && echo -e "  Gost 一键管理脚本 ${Red_font_prefix}[v${sh_ver}]${Font_color_suffix}
-	  
-	 ${Green_font_prefix}0.${Font_color_suffix} 升级脚本
+	echo && echo -e "Gost 一键管理脚本 ${Red_font_prefix}[v${sh_ver}]${Font_color_suffix}	
+ ${Green_font_prefix}0.${Font_color_suffix} 升级脚本
 
-	 ${Green_font_prefix}1.${Font_color_suffix} 安装 Gost
-	 ${Green_font_prefix}2.${Font_color_suffix} 升级 Gost
-	 ${Green_font_prefix}3.${Font_color_suffix} 卸载 Gost
-	————————————
-	 ${Green_font_prefix}4.${Font_color_suffix} 启动 Gost
-	 ${Green_font_prefix}5.${Font_color_suffix} 停止 Gost
-	 ${Green_font_prefix}6.${Font_color_suffix} 重启 Gost
-	————————————
-	 ${Green_font_prefix}7.${Font_color_suffix} 设置 Gost 账号
-	 ${Green_font_prefix}8.${Font_color_suffix} 查看 Gost 账号
-	 ${Green_font_prefix}9.${Font_color_suffix} 查看 Gost 日志
-	————————————" && echo
+ ${Green_font_prefix}1.${Font_color_suffix} 安装 Gost
+ ${Green_font_prefix}2.${Font_color_suffix} 升级 Gost
+ ${Green_font_prefix}3.${Font_color_suffix} 卸载 Gost
+————————————
+ ${Green_font_prefix}4.${Font_color_suffix} 启动 Gost
+ ${Green_font_prefix}5.${Font_color_suffix} 停止 Gost
+ ${Green_font_prefix}6.${Font_color_suffix} 重启 Gost
+————————————
+ ${Green_font_prefix}7.${Font_color_suffix} 设置 Gost 账号
+ ${Green_font_prefix}8.${Font_color_suffix} 查看 Gost 账号
+ ${Green_font_prefix}9.${Font_color_suffix} 查看 Gost 日志
+————————————" && echo
 	if [[ -e ${gost_file} ]]; then
 		check_pid
 		if [[ ! -z "${PID}" ]]; then
@@ -448,3 +443,4 @@ main(){
 		;;
 	esac
 }
+main
